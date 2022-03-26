636df94e3c75d4fb1258c9bba6e54c798f125d22
Pratikchitruk <99415272+Pratikchitruk@users.noreply.github.com>
2022-03-26 21:39:54 +0530

    PersonInfo.cs push for ValidateFunction

---
+++ PersonInfo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaValidate
+{
+    public class CustomerInfo
+    {
+        public string FirstName;
+        public string LastName;
+        public string Email;
+        public string PhoneNumber;
+        public string Password;
+        public bool IsValidByLamda(CustomerInfo customerInfo)
+        {
+            List<Func<CustomerInfo, bool>> rules = new List<Func<CustomerInfo, bool>>
+    {
+        x => String.IsNullOrEmpty(x.FirstName),
+        x => String.IsNullOrEmpty(x.LastName),
+        x => string.IsNullOrEmpty(x.Email),
+        x => String.IsNullOrEmpty(x.PhoneNumber),
+        x => String.IsNullOrEmpty(x.Password)
+    };
+
+            return rules.All(x => x(customerInfo) == false);
+        }
+    }
+}
