1c2e3a237a9bab47769076df91f8e55ca7f54c1a
PratikChitruk <pratik.chitruk@gmail.com>
2022-03-26 21:37:12 +0530

    Program.cs push for Validate funtion

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LambdaValidate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to lambda function");
+            CustomerInfo customerInfo = new CustomerInfo();
+            customerInfo.IsValidByLamda(customerInfo);
+            
+        }
+    }
+}
